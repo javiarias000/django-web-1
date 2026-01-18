72fd09150b1f3a0a1fbf82162a2707da8e601506
javi000 <javi000@javi000.local>
2026-01-18 02:00:53 -0500

    feat: Add caching for GitHub API and load environment variables

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv # Import load_dotenv
+import os # Import os module
+
+load_dotenv() # Load environment variables from .env file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
